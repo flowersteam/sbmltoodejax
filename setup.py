4991979443cbb06ed5dd1e0ac70e772c74efaceb
Mayalen Etcheverry <etcheverry.mayalen@gmail.com>
2023-02-23 13:37:50 +0100

    setup.py for preparing the pip package

---
+++ setup.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='sbmltoodejax',
+    version='0.1',
+    author='Mayalen Etcheverry',
+    author_email='mayalen.etcheverry@inria.fr',
+    description='python software built upon jax, that allows to convert SBML models into python classes',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/flowersteam/autodiscjax',
+    license='MIT',
+    packages=['sbmltoodejax'],
+    install_requires=['sbmltoodepy', 'jax[cpu]', 'equinox'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
