c8f8631e0c1a981fd5b3cd9c036422a9e5d8305e
Mayalen Etcheverry <etcheverry.mayalen@gmail.com>
2023-05-19 09:19:36 +0000

    changed year to 2023

--- sbmltoodejax/__init__.py
+++ sbmltoodejax/__init__.py
@@ -3,11 +3,11 @@ import sbmltoodejax.jaxfuncs
 import sbmltoodejax.modulegeneration
 import sbmltoodejax.parse
 
-__version__ = "0.1"
+__version__ = "0.2"
 
 __author__ = "Mayalen Etcheverry"
 __email__ = "mayalen.etcheverry@inria.fr"
 __uri__ = "https://github.com/flowersteam/sbmltoodejax"
 __license__ = "MIT"
-__copyright__ = "2022, Mayalen Etcheverry"
+__copyright__ = "2023, Mayalen Etcheverry"
 
