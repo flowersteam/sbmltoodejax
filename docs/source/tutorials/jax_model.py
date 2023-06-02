429d7fc9559ddc6e27fb4213d28e9e21fcf4497a
Mayalen Etcheverry <etcheverry.mayalen@gmail.com>
2023-06-02 18:12:25 +0000

    update tutorials with externalized load_biomodel function (now part of sbmltoodejax.utils), minor changes in the codebase, addition of explanatory text

--- docs/source/tutorials/jax_model.py
+++ docs/source/tutorials/jax_model.py
@@ -8,21 +8,21 @@ from sbmltoodejax import jaxfuncs
 
 t0 = 0.0
 
-y0 = jnp.array([0.5, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0])
-y_indexes = {'R': 0, 'Rin': 1, 'x1': 2, 'x1p': 3, 'x2': 4, 'x2p': 5, 'x3': 6, 'x3p': 7}
+y0 = jnp.array([1.0, 9.0, 1.0, 1000.0, 200.0])
+y_indexes = {'Galpha_GTP': 0, 'APLC': 1, 'IP3': 2, 'Ca_ER': 3, 'Ca_Cyt': 4}
 
-w0 = jnp.array([])
-w_indexes = {}
+w0 = jnp.array([1.0, 0.6923076923076923, 0.045454545454545456, 2.5599934464167773e-06, 0.009900990099009901, 6.399590426212723e-05, 0.16666666666666666, 0.09090909090909091, 0.9995783029034626, 1.0])
+w_indexes = {'DG': 0, 'Raplc': 1, 'Rpkc': 2, 'Rgalpha_gtp': 3, 'Rdg': 4, 'Rip3': 5, 'Rcyt1': 6, 'Rcyt2': 7, 'Rer': 8, 'PLC': 9}
 
-c = jnp.array([1.0, 1.0, 0.1, 0.01, 0.1, 1.0, 0.1, 0.3, 1.0, 1.0, 0.1, 0.3, 1.0, 1.0, 0.1, 0.3, 1.0, 0.0, 1.0]) 
-c_indexes = {'compartment': 0, 'v1_Vm1': 1, 'v1_Km1': 2, 'v2_Vm2': 3, 'v2_Km2': 4, 'v3_k3': 5, 'v3_Km3': 6, 'v4_Vm4': 7, 'v4_Km4': 8, 'v5_k5': 9, 'v5_Km5': 10, 'v6_Vm6': 11, 'v6_Km6': 12, 'v7_k7': 13, 'v7_Km7': 14, 'v8_Vm8': 15, 'v8_Km8': 16, 'v8_Inh': 17, 'v8_Ki8': 18}
+c = jnp.array([4.0, 10.0, 200.0, 4.0, 25.0, 2.0, 25.0, 1000.0, 2000.0, 3.0, 75.0, 10.0, 0.1, 3.4, 4.0, 4.5, 1.2, 0.12, 14.0, 2.0, 10500.0, 600.0, 3000.0, 260.0, 1.0, 1.0]) 
+c_indexes = {'Kp': 0, 'Kd': 1, 'Kr': 2, 'n': 3, 'Kg': 4, 'm': 5, 'Ks': 6, 'Kc1': 7, 'Kc2': 8, 'w': 9, 'Ker': 10, 'Cplc_total': 11, 'k0': 12, 'k1': 13, 'k2': 14, 'k3': 15, 'k4': 16, 'k5': 17, 'k6': 18, 'k7': 19, 'k8': 20, 'k9': 21, 'k10': 22, 'k11': 23, 'Cytosol': 24, 'ER': 25}
 
 class RateofSpeciesChange(eqx.Module):
-	stoichiometricMatrix = jnp.array([[-1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [1.0, -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, -1.0, 1.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 1.0, -1.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, -1.0, 1.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 1.0, -1.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 1.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, -1.0]], dtype=jnp.float32) 
+	stoichiometricMatrix = jnp.array([[1.0, 1.0, -1.0, -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 1.0, -1.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, -1.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.0010000000474974513, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.009999999776482582, -0.05000000074505806, 0.05000000074505806]], dtype=jnp.float32) 
 
 	@jit
 	def __call__(self, y, t, w, c):
-		rateRuleVector = jnp.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], dtype=jnp.float32)
+		rateRuleVector = jnp.array([0.0, 0.0, 0.0, 0.0, 0.0], dtype=jnp.float32)
 
 		reactionVelocities = self.calc_reaction_velocities(y, w, c, t)
 
@@ -32,45 +32,77 @@ class RateofSpeciesChange(eqx.Module):
 
 
 	def calc_reaction_velocities(self, y, w, c, t):
-		reactionVelocities = jnp.array([self.v1(y, w, c, t), self.v2(y, w, c, t), self.v3(y, w, c, t), self.v4(y, w, c, t), self.v5(y, w, c, t), self.v6(y, w, c, t), self.v7(y, w, c, t), self.v8(y, w, c, t)], dtype=jnp.float32)
+		reactionVelocities = jnp.array([self.R1(y, w, c, t), self.R2(y, w, c, t), self.R3(y, w, c, t), self.R4(y, w, c, t), self.R5(y, w, c, t), self.R6(y, w, c, t), self.R7(y, w, c, t), self.R8(y, w, c, t), self.R9(y, w, c, t), self.R10(y, w, c, t), self.R11(y, w, c, t)], dtype=jnp.float32)
 
 		return reactionVelocities
 
 
-	def v1(self, y, w, c, t):
-		return c[1] * y[0] / (c[2] + y[0])
+	def R1(self, y, w, c, t):
+		return c[24] * c[12]
 
 
-	def v2(self, y, w, c, t):
-		return c[3] * y[1] / (c[4] + y[1])
+	def R2(self, y, w, c, t):
+		return c[24] * c[13] * (y[0]/1.0)
 
 
-	def v3(self, y, w, c, t):
-		return c[5] * y[0] * y[2] / (c[6] + y[2])
+	def R3(self, y, w, c, t):
+		return c[24] * c[14] * w[1] * (y[0]/1.0)
 
 
-	def v4(self, y, w, c, t):
-		return c[7] * y[3] / (c[8] + y[3])
+	def R4(self, y, w, c, t):
+		return c[24] * c[15] * w[2] * (y[0]/1.0)
 
 
-	def v5(self, y, w, c, t):
-		return c[9] * y[3] * y[4] / (c[10] + y[4])
+	def R5(self, y, w, c, t):
+		return c[24] * c[16] * w[3] * w[4] * (w[9]/1.0)
 
 
-	def v6(self, y, w, c, t):
-		return c[11] * y[5] / (c[12] + y[5])
+	def R6(self, y, w, c, t):
+		return c[24] * c[17] * (y[1]/1.0)
 
 
-	def v7(self, y, w, c, t):
-		return c[13] * y[5] * y[6] / (c[14] + y[6])
+	def R7(self, y, w, c, t):
+		return c[24] * c[18] * (y[1]/1.0)
 
 
-	def v8(self, y, w, c, t):
-		return c[15] * y[7] / c[16] / (1 + y[7] / c[16] + c[17] / c[18])
+	def R8(self, y, w, c, t):
+		return c[24] * c[19] * (y[2]/1.0)
+
+
+	def R9(self, y, w, c, t):
+		return c[25] * (c[20] * w[5] * w[8] - c[21] * w[6])
+
+
+	def R10(self, y, w, c, t):
+		return c[24] * c[22] * w[7]
+
+
+	def R11(self, y, w, c, t):
+		return c[24] * c[23]
 
 class AssignmentRule(eqx.Module):
 	@jit
 	def __call__(self, y, w, c, t):
+		w = w.at[0].set(1.0 * ((y[2]/1.0)))
+
+		w = w.at[1].set(((y[1]/1.0) / (c[0] + (y[1]/1.0))))
+
+		w = w.at[2].set((((w[0]/1.0) / (c[1] + (w[0]/1.0))) * (y[4]/1.0) / (c[2] + (y[4]/1.0))))
+
+		w = w.at[3].set(((y[0]/1.0)**c[3] / (c[4]**c[3] + (y[0]/1.0)**c[3])))
+
+		w = w.at[4].set(((w[0]/1.0)**c[5] / (c[1]**c[5] + (w[0]/1.0)**c[5])))
+
+		w = w.at[5].set(((y[2]/1.0)**3 / (c[6]**3 + (y[2]/1.0)**3)))
+
+		w = w.at[6].set(((y[4]/1.0) / (c[7] + (y[4]/1.0))))
+
+		w = w.at[7].set(((y[4]/1.0) / (c[8] + (y[4]/1.0))))
+
+		w = w.at[8].set(((y[3]/1.0)**c[9] / (c[10]**c[9] + (y[3]/1.0)**c[9])))
+
+		w = w.at[9].set(1.0 * (c[11] - (y[1]/1.0)))
+
 		return w
 
 class ModelStep(eqx.Module):
@@ -83,7 +115,7 @@ class ModelStep(eqx.Module):
 	mxstep: int = eqx.static_field()
 	assignmentfunc: AssignmentRule
 
-	def __init__(self, y_indexes={'R': 0, 'Rin': 1, 'x1': 2, 'x1p': 3, 'x2': 4, 'x2p': 5, 'x3': 6, 'x3p': 7}, w_indexes={}, c_indexes={'compartment': 0, 'v1_Vm1': 1, 'v1_Km1': 2, 'v2_Vm2': 3, 'v2_Km2': 4, 'v3_k3': 5, 'v3_Km3': 6, 'v4_Vm4': 7, 'v4_Km4': 8, 'v5_k5': 9, 'v5_Km5': 10, 'v6_Vm6': 11, 'v6_Km6': 12, 'v7_k7': 13, 'v7_Km7': 14, 'v8_Vm8': 15, 'v8_Km8': 16, 'v8_Inh': 17, 'v8_Ki8': 18}, atol=1e-06, rtol=1e-12, mxstep=5000000):
+	def __init__(self, y_indexes={'Galpha_GTP': 0, 'APLC': 1, 'IP3': 2, 'Ca_ER': 3, 'Ca_Cyt': 4}, w_indexes={'DG': 0, 'Raplc': 1, 'Rpkc': 2, 'Rgalpha_gtp': 3, 'Rdg': 4, 'Rip3': 5, 'Rcyt1': 6, 'Rcyt2': 7, 'Rer': 8, 'PLC': 9}, c_indexes={'Kp': 0, 'Kd': 1, 'Kr': 2, 'n': 3, 'Kg': 4, 'm': 5, 'Ks': 6, 'Kc1': 7, 'Kc2': 8, 'w': 9, 'Ker': 10, 'Cplc_total': 11, 'k0': 12, 'k1': 13, 'k2': 14, 'k3': 15, 'k4': 16, 'k5': 17, 'k6': 18, 'k7': 19, 'k8': 20, 'k9': 21, 'k10': 22, 'k11': 23, 'Cytosol': 24, 'ER': 25}, atol=1e-06, rtol=1e-12, mxstep=5000000):
 
 		self.y_indexes = y_indexes
 		self.w_indexes = w_indexes
@@ -112,7 +144,7 @@ class ModelRollout(eqx.Module):
 		self.modelstepfunc = ModelStep(atol=atol, rtol=rtol, mxstep=mxstep)
 
 	@partial(jit, static_argnames=("n_steps",))
-	def __call__(self, n_steps, y0=jnp.array([0.5, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0]), w0=jnp.array([]), c=jnp.array([1.0, 1.0, 0.1, 0.01, 0.1, 1.0, 0.1, 0.3, 1.0, 1.0, 0.1, 0.3, 1.0, 1.0, 0.1, 0.3, 1.0, 0.0, 1.0]), t0=0.0):
+	def __call__(self, n_steps, y0=jnp.array([1.0, 9.0, 1.0, 1000.0, 200.0]), w0=jnp.array([1.0, 0.6923076923076923, 0.045454545454545456, 2.5599934464167773e-06, 0.009900990099009901, 6.399590426212723e-05, 0.16666666666666666, 0.09090909090909091, 0.9995783029034626, 1.0]), c=jnp.array([4.0, 10.0, 200.0, 4.0, 25.0, 2.0, 25.0, 1000.0, 2000.0, 3.0, 75.0, 10.0, 0.1, 3.4, 4.0, 4.5, 1.2, 0.12, 14.0, 2.0, 10500.0, 600.0, 3000.0, 260.0, 1.0, 1.0]), t0=0.0):
 
 		@jit
 		def f(carry, x):
