a0bc3508cab001bab80e8e32a0fca9c1b2629190
Mayalen Etcheverry <etcheverry.mayalen@gmail.com>
2023-06-06 15:19:47 +0000

    pypi release 0.3.1

--- sbmltoodejax/__init__.py
+++ sbmltoodejax/__init__.py
@@ -3,11 +3,11 @@ import sbmltoodejax.jaxfuncs
 import sbmltoodejax.modulegeneration
 import sbmltoodejax.parse
 
-__version__ = "0.2"
+__version__ = "0.3.1"
 
 __author__ = "Mayalen Etcheverry"
 __email__ = "mayalen.etcheverry@inria.fr"
-__uri__ = "https://github.com/flowersteam/sbmltoodejax"
+__url__ = "https://github.com/flowersteam/sbmltoodejax"
 __license__ = "MIT"
 __copyright__ = "2023, Mayalen Etcheverry"
 
--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 #! /usr/bin/python3
 import setuptools
+import sbmltoodejax
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='sbmltoodejax',
-    version='0.2',
-    author='Mayalen Etcheverry',
-    author_email='mayalen.etcheverry@inria.fr',
-    description='python software built upon jax, that allows to convert SBML models into python classes',
+    version=sbmltoodejax.__version__,
+    author=sbmltoodejax.__author__,
+    author_email=sbmltoodejax.__email__,
+    description='lightweight library that allows to automatically parse and convert SBML models into python models written end-to-end in JAX',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/flowersteam/autodiscjax',
-    license='MIT',
+    url=sbmltoodejax.__url__,
+    license=sbmltoodejax.__license__,
     packages=['sbmltoodejax'],
     install_requires=['sbmltoodepy', 'jax[cpu]', 'equinox'],
     classifiers=[
