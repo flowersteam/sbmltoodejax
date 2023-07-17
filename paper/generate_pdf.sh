77cf974936e5edd0c85e99542458460b6a487771
Mayalen Etcheverry <etcheverry.mayalen@gmail.com>
2023-07-17 12:41:20 +0000

    added generation of paper.tex

--- paper/generate_pdf.sh
+++ paper/generate_pdf.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Generate PDF from markdown
+pandoc paper.md --citeproc --lua-filter=scholarly-metadata.lua --lua-filter=author-info-blocks.lua --pdf-engine=xelatex --read=markdown --write=latex --standalone -o paper.tex
 pandoc paper.md --citeproc --lua-filter=scholarly-metadata.lua --lua-filter=author-info-blocks.lua --pdf-engine=xelatex -o paper.pdf
